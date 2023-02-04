aaf6029ed55d7da361a05f008f6f9409e6570581
nikomv <v40830314qqq@gmail.com>
2023-02-04 18:10:09 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+const PEOPLE_LIST = [
+  {
+    name: "Иван",
+    specialist: "Врач"
+  },
+  {
+    name: "Владимир",
+    specialist: "Преподаватель"
+  },
+  {
+    name: "Виталий",
+    specialist: "Студент"
+  }
+];
+const ROOT = document.getElementById("app");
